1df5519d90bc8bdaa034fff4a31e548b39dc40d9
Argem Flores <argemflores@gmail.com>
2022-10-08 14:34:27 +0800

    Create login view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -44,3 +44,48 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    """Login user
+
+    Returns:
+        str: Login page
+    """
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        database = get_db()
+        error = None
+        user = database.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """Load logged in user
+    """
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
