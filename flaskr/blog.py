92e2e0bf3d52f913bbf43bb62150d6dfca5f43bc
Argem Flores <argemflores@gmail.com>
2022-10-08 16:04:25 +0800

    Create create post template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,3 +8,34 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    """Create blog
+
+    Returns:
+        str: Create blog page
+    """
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            database = get_db()
+            database.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            database.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
