fbdc2a91fcb76231db2cebf2d2a74bd199a81695
Argem Flores <argemflores@gmail.com>
2022-10-08 16:08:36 +0800

    Create update post template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -39,3 +39,65 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+def get_post(post_id, check_author=True):
+    """Get post
+
+    Args:
+        post_id (int): Post ID
+        check_author (bool, optional): Check if author matches the logged in user. Defaults to True.
+
+    Returns:
+        object: Post
+    """
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (post_id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {post_id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(post_id):
+    """Update post
+
+    Args:
+        post_id (int): Post ID
+
+    Returns:
+        str: Update page
+    """
+    post = get_post(post_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            database = get_db()
+            database.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, post_id)
+            )
+            database.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
