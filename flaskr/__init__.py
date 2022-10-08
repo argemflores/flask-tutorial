c72f869bf63968b22a7704a3d5335bc83c294a73
Argem Flores <argemflores@gmail.com>
2022-10-08 15:50:01 +0800

    Create blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 from . import db
 from . import auth
+from . import blog
 
 
 def create_app(test_config=None):
@@ -43,5 +44,7 @@ def create_app(test_config=None):
 
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
